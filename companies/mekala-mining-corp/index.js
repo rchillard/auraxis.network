43b5017ce498065938caadccb8eeaa62c0e74b51
RCH <rchillard@gmail.com>
2018-12-09 22:17:42 -0600

    Styling updates and carousel overlays added for Mekala Mining Corp site

--- companies/mekala-mining-corp/index.js
+++ companies/mekala-mining-corp/index.js
@@ -1,10 +1,21 @@
-$(function() {
-  $(document).scroll(function() {
-    var $nav = $("#mainNavbar");
-    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-  });
+// $(function() {
+//   $(document).scroll(function() {
+//     var $nav = $("#mainNavbar");
+//     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+//   });
+// });
+
+// Does not work yet and not quite sure why
+$(".nav-link").click(function() {
+  var sectionTo = $(this).attr("href");
+  $("html, body").animate(
+    {
+      scrollTop: $(sectionTo).offset().top
+    },
+    1500
+  );
 });
 
 $(".carousel").carousel({
-  interval: 30000
+  interval: 15000
 });
